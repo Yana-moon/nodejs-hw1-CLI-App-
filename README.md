e62a99184feca77a0a2f0d6ebdf1f30594308398
Яна <112972800+Yana-moon@users.noreply.github.com>
2023-06-05 09:55:30 +0300

    done hw

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # nodejs-hw1-CLI-App-
+
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+![List contacts](./screenshots/listContacts.png)
+
+# Отримуємо контакт по id
+![Get contacts](./screenshots/getContacts.png)
+
+# Додаємо контакт
+![Add contacts](./screenshots/addContacts.png)
+
+# Видаляємо контакт
+![Remove contacts](./screenshots/removeContacts.png)
+
+# Список контактів після всіх дій
+![After all actions](./screenshots/afterAllActions.png)
---
+++ contacts.js
@@ -0,0 +1,64 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getContactById = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const resultContactById = contacts.find(
+      (contact) => contact.id === contactId
+    );
+    return resultContactById || null;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const removeContact = async (contactId) => {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    //if (index === -1) return null;
+
+    const [deleteContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, 2));
+    return deleteContact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const addContact = async (name, email, phone) => {
+  try {
+    const contacts = await listContacts();
+    const newContact = {
+      id: nanoid(),
+      name,
+      email,
+      phone,
+    };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
